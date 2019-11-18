9d241ecee0fc8a8db3728b9b1ddaac91daaecba5
Alba Olveros <aolveros@netsuite.com>
2019-11-18 14:41:18 +0100

    Added the follwing:
    - Pending tag for each new element;
    - When clicking Add to List button, clear the field and put the cursor there;
    - When clicking the Pending label, remove the Pending label for that list element.

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 /* Exercise 1: Wish list */
 function addToList(cosita) {
     $('#items').append('<li>' + cosita + '</li>');
+    $('#items').append('<span class=label-pending>' + 'Pending' + '</span>');
 }
+
+$(document).on('click', '#add-to-list', function() {
+    addToList($('#item').val());
+    $('#item').focus().val('');
+});
+
+$(document).on('click', '.label-pending', function() {
+    var li_node = $(this).parent();
+    $(this).remove('.label-pending');
+    //$(this).addClass('completed');
+    //$(this).append('<span class=label-success>' + 'Done!' + '</span>');
+});
